0702c23f1456f9ced61ea2ba67d574cd7baa4851
Kronos163 <32158397+Kronos163@users.noreply.github.com>
2017-09-27 18:22:16 +0400

    Added entry point

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+package PO51.Menchin.wdad;
+
+/**
+ * Created by Kronos on 27.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("i, Menchin Dmitry, not a monkey anymore)");
+    }
+}
